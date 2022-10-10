3c0abf0a7a44c2b531750cb8eeaef4b7683be391
Melissa McFadzien <mmcf647@aucklanduni.ac.nz>
2022-10-11 12:14:18 +1300

    Url pre-fill form fields - not complete

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,11 +2,11 @@
 window.addEventListener("load", async function() {
 
 const emailArea = document.querySelector("#emailArea");
+var manualEmailDiv = document.querySelector("#manualEmailEntry");
 
 let recieverEmail = await lookForEmailAddress();
 if (recieverEmail == null) {
   recieverEmail = "No contact email found, please add one below!";
-  var manualEmailDiv = document.querySelector("#manualEmailEntry");
   manualEmailDiv.style.display = `block`;
 } 
 
@@ -14,6 +14,7 @@ const emailField = document.querySelector("#sendingToEmail");
 emailField.innerHTML = `<p> ${recieverEmail} </p> `;
 
 const altTextField = document.querySelector("#altTextBox");
+var manualEmail = document.querySelector("#manualEmail");
 
 
 const popUpTitle = document.querySelector("#popupNavBar");
@@ -26,8 +27,9 @@ const editEmailLink = document.querySelector("#editEmailLink");
 editEmailLink.addEventListener("click", async function() {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
     const altTextInput = altTextField.value;
+    const emailAddress = manualEmail.value;
     const url = tabs[0].url;
-    const website = `https://altit.netlify.app/#/suggestion?url=${url}&message=${altTextInput}`;
+    const website = `https://altit.netlify.app/#/suggestion?url=${url}&message=${altTextInput}&user_email=${emailAddress}`;
     localStorage.setItem("altText", altTextInput);
     chrome.tabs.create({ url: website });
   });
