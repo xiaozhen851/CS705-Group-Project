31619ea9286c9c3aa578427909a11ff249d3339c
xiaozhen <xli851@aucklanduni.ac.nz>
2022-10-11 16:40:39 +1300

    Modified homepage

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 
 ## Contents  
 
-This project consists of two parts - the browser extension and the webpage.  
-Please clone the entire repository and follow the below directions to view the project.  
-The webpage is available live at <https://altit.netlify.app/#/>
+This project consists of two parts - the browser extension and the web application.  
+Please clone the entire repository [here](https://github.com/xiaozhen851/CS705-Group-Project) and follow the below directions to view the project.  
+The web app is available live at <https://altit.netlify.app/>
 
 ---
 
@@ -15,7 +15,7 @@ The webpage is available live at <https://altit.netlify.app/#/>
 - Select 'Manage Extensions' at the bottom of the drop-down list.
 - Check that 'Developer Mode' in the top right corner is toggled ON.  
   
-  ![Screenshot of chrome extension manager](developerModeimage.png)
+  ![Screenshot of chrome extension manager](./src/assets/developerModeimage.png)
 - Select 'Load unpacked' and choose the 'src' folder.
 - If successfully added, the extension should now appear in the list that opens when the puzzle piece icon is clicked, select 'Alt Text Checker' to open the pop-up.
 - Optional: On the drop down extensions menu, you can select the push pin icon to keep the Alt It extension in your toolbar.
@@ -31,34 +31,4 @@ We suggest entering your own email address as the recipient so that you can see 
 Notes about using the extension
 
 - Please ensure the extension is installed before opening the webpage you wish to use it on.
-- The pop-up form does not allow input for pages with no valid images found.  
-
----
-
-## Project setup
-
-``` command
-npm install
-```
-
-### Compiles and hot-reloads for development
-
-``` command
-npm run serve
-```
-
-### Compiles and minifies for production
-
-``` command
-npm run build
-```
-
-### Lints and fixes files
-
-``` command
-npm run lint
-```
-
-### Customize configuration
-
-See [Configuration Reference](https://cli.vuejs.org/config/).
+- The pop-up form does not allow input for pages with no valid images found.
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,16 +6,24 @@
           Alt it!
           <span>Suggest alt text for images</span>
         </div>
-        <top-bar class="top-bar">
-          <email-dialog />
-        </top-bar>
+        <div class="top-bar">
+          <el-button type="text" icon="el-icon-info" @click="goAbout">About</el-button>
+          <el-button type="text" @click="goSuggestion" icon="el-icon-message">Send a suggestion</el-button>
+        </div>
+<!--        <top-bar class="top-bar">-->
+<!--          <email-dialog />-->
+<!--        </top-bar>-->
       </el-header>
       <el-container>
         <el-aside width="300px">
-          <history-comp />
+          <el-button type="primary" @click="goAbout">About alt text and Alt it!</el-button>
+          <br />
+          <el-button type="primary" @click="goSuggestion">Submit an alt text suggestion</el-button>
+          <br />
+          <el-button type="primary" @click="goToSourceCode">Source code and the extension</el-button>
         </el-aside>
         <el-main>
-          <image-list />
+          <introduction-view />
         </el-main>
       </el-container>
     </el-container>
@@ -24,18 +32,27 @@
 
 <script>
 // @ is an alias to /src
-import EmailDialog from '@/components/EmailDialog.vue'
-import TopBar from '@/components/TopBar';
-import ImageList from '@/components/ImageList';
-import HistoryComp from '@/components/HistoryComp';
+// import EmailDialog from '@/components/EmailDialog.vue'
+// import TopBar from '@/components/TopBar';
+// import ImageList from '@/components/ImageList';
+// import HistoryComp from '@/components/HistoryComp';
+import IntroductionView from '@/views/IntroductionView';
 
 export default {
   name: 'HomeView',
   components: {
-    EmailDialog,
-    TopBar,
-    ImageList,
-    HistoryComp
+    IntroductionView
+  },
+  methods: {
+    goAbout() {
+      this.$router.push('/about');
+    },
+    goSuggestion() {
+      this.$router.push('/suggestion');
+    },
+    goToSourceCode() {
+      window.open('https://github.com/xiaozhen851/CS705-Group-Project', '_blank');
+    }
   }
 }
 </script>
@@ -56,9 +73,20 @@ export default {
 
 .top-bar {
   background: rgb(217, 236, 255);
+  text-align: right;
+  padding: 8px 20px;
 }
 
 .el-header {
   padding: 0;
+}
+
+.el-aside {
+  padding: 20px;
+}
+
+.el-aside .el-button {
+  width: 100%;
+  margin-bottom: 20px;
 }
 </style>
